b749fef0438df6ee84cc9a356ba9a13b53f0ba15
Positive Mindset <positive88mindset@gmail.com>
2021-12-15 16:23:28 -0500

    listing items on cart page

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 
 import { Container, Typography, Button, Grid } from '@material-ui/core';
 import useStyles from './cartStyles';
+import CartItem from './CartItem/CartItem';
+
+
 
 const Cart = ({cart}) => {
     
@@ -23,7 +26,7 @@ const Cart = ({cart}) => {
         {cart.line_items.map((item)=>(
             <Grid item xs={12} sm={4} key={item.id}>
 
-            <div>{item.name}</div>
+            <CartItem item={item}/>
 
             </Grid>
         ))}
@@ -56,7 +59,7 @@ const Cart = ({cart}) => {
         <Container>
             <div className='classes.toolbar' />
 
-            <Typography className='classes.title' variant='h3'>Your Shopping Cart
+            <Typography className='classes.title' variant='h3' gutterBottom>Your Shopping Cart
             </Typography>
 
             { isEmpty ? <EmptyCart /> : <FilledCart /> }
---
+++ src/components/Cart/CartItem/CartItem.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Typography, Button, Card, CardActions, CardContent, CardMedia} from '@material-ui/core';
+
+import useStyles from './ItemStyles';
+
+const CartItem = ({ item }) => {
+
+const classes= useStyles();
+
+
+    return (
+        <div>
+
+        <Card>
+
+        <CardMedia image={item.image.url} alt={item.name} className={classes.media}
+        />
+
+        <CardContent className={classes.cardContent}>
+        <Typography variant='h4' >{item.name}</Typography>
+        <Typography variant='h5' >{item.line_total.formatted_with_symbol}</Typography>
+        </CardContent>
+
+        <CardActions className={classes.CardActions}>
+        <div className={classes.buttons}>
+            <Button type='button' size='small'>-</Button>
+            <Typography>{item.quantity}</Typography>
+            <Button type='button' size='small'>+</Button>
+        </div>
+
+        <Button variant='contained' type='button' color='secondary'>Remove</Button>
+
+
+        </CardActions>
+
+
+
+        </Card>
+
+
+
+
+            
+        </div>
+    )
+}
+
+export default CartItem
